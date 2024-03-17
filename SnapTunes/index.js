54b145c1f5e5eaf0de94589268297974cbae885b
ethanthankim <ethan.kim1996@gmail.com>
2024-03-17 18:57:16 -0400

    base

---
+++ SnapTunes/index.js
@@ -0,0 +1,25 @@
+const express= require('express');
+const app = express();
+app.use(express.static('public'));
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('music',(melody)=>{
+        io.emit('music',melody);
+    });
+    socket.on('disconnect',() => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000,() => {
+    console.log('listening on *:3000');
+});
